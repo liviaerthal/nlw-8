334e65d7cd52f018fae510bfb7948d60616bd212
Lívia Erthal <liviammerthal@gmail.com>
2022-05-05 14:05:51 -0300

    aula 4

--- main.js
+++ main.js
@@ -1,25 +1,37 @@
 
-function onScroll() {
+window.addEventListener('scroll', onScroll);
 
+onScroll();
+function onScroll() {
+    showNavOnScroll();
+    showBackToTopButtonOnScroll();
+}
+
+function showNavOnScroll() {
     if (scrollY > 0) {
         navigation.classList.add('scroll')
     }
-
     else {
         navigation.classList.remove('scroll')
     }
 }
 
-function openMenu(){
+function showBackToTopButtonOnScroll() {
+    if (scrollY > 550) {
+        backToTopButton.classList.add('show')
+    }
+    else {
+        backToTopButton.classList.remove('show')
+    }
+}
+
+function openMenu() {
     document.body.classList.add('menu-expanded');
 }
 
-function closeMenu(){
+function closeMenu() {
     document.body.classList.remove('menu-expanded');
 }
-
-
-
 
 ScrollReveal({
     origin: 'top',
